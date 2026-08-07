550bbf52262b7c508b32755101a88802d32d84b0
Revana Clarestya <reva.reane@gmail.com>
2026-08-07 13:19:34 +0700

    Update date in config.rs file header

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,6 +1,9 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
+//!
 //! Author  : Revana
-//! Date    : 06/08/2026
+//! Date    : 07/08/2026
+//! Files   : nexsiz/src/common/config.rs
+//!
 //! Configuration management for Nexsiz.
 
 use crate::common::error::{NexsizError, Result};
