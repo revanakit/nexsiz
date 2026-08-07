c345f54300269648758bc19328702c8b4264f514
Revana Clarestya <reva.reane@gmail.com>
2026-08-07 13:12:20 +0700

    Update author date in json.rs documentation

--- src/scripting/json.rs
+++ src/scripting/json.rs
@@ -1,7 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
 //! Author  : Revana
-//! Date    : 06/08/2026
+//! Date    : 07/08/2026
 //! Files   : nexsiz/src/scripting/json.rs
 //!
 //! Minimal JSON helpers for the RPC control plane (no serde).
