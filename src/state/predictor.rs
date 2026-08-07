e68aa7a09b7cd5d15dbe2c7fef622ece21f6468b
Revana Clarestya <reva.reane@gmail.com>
2026-08-07 09:25:42 +0700

    Update author date in predictor.rs file

--- src/state/predictor.rs
+++ src/state/predictor.rs
@@ -1,6 +1,9 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
+//!
 //! Author  : Revana
-//! Date    : 04/08/2026
+//! Date    : 06/08/2026
+//! Files   : nexsiz/src/state/predictor.rs
+//!
 //! Adaptive state transition predictor.
 
 use std::collections::HashMap;
