24a634722e35ffeab87a54b9377ea009c83609d8
Revana Clarestya <reva.reane@gmail.com>
2026-08-07 13:11:00 +0700

    Update date in encryptor_bridge.rs documentation

--- src/scripting/encryptor_bridge.rs
+++ src/scripting/encryptor_bridge.rs
@@ -1,7 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
 //! Author  : Revana
-//! Date    : 06/08/2026
+//! Date    : 07/08/2026
 //! Files   : nexsiz/src/scripting/encryptor_bridge.rs
 //!
 //! Python selects an encryptor by name and optional key material.
