2c22481311ee57e6171c592cdc54a7ab6e14ecea
Revana Clarestya <reva.reane@gmail.com>
2026-08-09 09:17:21 +0700

    Update date in config.rs documentation

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,7 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
 //! Author  : Revana
-//! Date    : 08/08/2026
+//! Date    : 09/08/2026
 //! Files   : nexsiz/src/common/config.rs
 //!
 //! NEXSIZ Configuration Module
