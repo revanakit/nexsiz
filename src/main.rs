3a12620c91fe620ad7855530857b09c1bbf18d6c
Revana Clarestya <reva.reane@gmail.com>
2026-08-14 15:40:51 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
+//!---------------------------------
 //! Author  : Revana
-//! Date    : 09/08/2026
 //! Module  : nexsiz::src::main
+//! --------------------------------
 //!
 //! Short description:
 //!   Entry point for Nexsiz. Implements a compact, dependency-free CLI
