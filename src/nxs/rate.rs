6b88ab207ac975c0cb2644c923cbb33c15be78bc
Revana Clarestya <reva.reane@gmail.com>
2026-08-14 11:22:27 +0700

    Add module documentation for rate limiting

--- src/nxs/rate.rs
+++ src/nxs/rate.rs
@@ -1,3 +1,9 @@
+//! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
+//!
+//! Author  : Revana
+//! Date    : 09/08/2026
+//! Module  : nexsiz::src::nxs::rate
+//!
 //! NXS spawn rate limiting and deduplication
 //!
 //! Process-local ledger:
