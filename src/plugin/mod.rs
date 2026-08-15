2021155435afafc288d03c5c7a9bd984a360a889
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:09:49 +0700

    Update module documentation header format

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 08/08/2026
-//! Files   : nexsiz/src/plugin/mod.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::plugin::mod
 //!
 //! NEXSIZ – Plugin architecture
 //! 
