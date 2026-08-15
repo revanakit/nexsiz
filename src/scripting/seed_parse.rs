c77d305efe6bfdd3d43ff2bf613413ec9ea6c199
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:21:21 +0700

    Revise documentation in seed_parse.rs
    Updated author and module documentation comments.

--- src/scripting/seed_parse.rs
+++ src/scripting/seed_parse.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 07/08/2026
-//! Files   : nexsiz/src/scripting/seed_parse.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::scripting::seed_parse
 //!
 //! Parse structured seed JSON into TestCase
 
