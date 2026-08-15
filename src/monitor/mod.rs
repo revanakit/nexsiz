bad1a43c74ee5bf4642250e87fd032c004e5782c
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 12:59:33 +0700

    Update module documentation header format

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -1,6 +1,8 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
-//! Author  : Revana
-//! Date    : 04/08/2026
+//!
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::monitor::mod
+//!
 
 pub mod logger;
 pub mod minimizer;
