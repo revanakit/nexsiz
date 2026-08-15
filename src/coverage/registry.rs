6391fdd89db4b6d134e240943caef7fd7823e421
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 10:54:18 +0700

    Update registry.rs

--- src/coverage/registry.rs
+++ src/coverage/registry.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 12/08/2026
-//! Module  : nexsiz::src::coverage::registry
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::coverage::registry
 //!
 //! Coverage Provider Registry
 //! --------------------------
