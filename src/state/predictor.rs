b6a42ec100d0a3170a50f78bebc4fbbcb24cbd05
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:13:59 +0700

    Update predictor.rs

--- src/state/predictor.rs
+++ src/state/predictor.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 07/08/2026
-//! Files   : nexsiz/src/state/predictor.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::state::predictor
 //!
 //! Adaptive state transition predictor.
 
