1496d7e7f2647a5a5fd79bda85e11bf1431438ab
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 10:54:54 +0700

    Update shm.rs

--- src/coverage/shm.rs
+++ src/coverage/shm.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 12/08/2026
-//! Module  : nexsiz::src::coverage::shm
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::coverage::shm
 //!
 //! Compatibility layer for POSIX Shared-Memory Coverage Map.
 //!
