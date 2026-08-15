fdd7baacc0309815c28505508b567aaec4eda3e5
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:18:15 +0700

    Revise documentation header in mod.rs
    Updated author and module documentation in mod.rs.

--- src/scripting/mod.rs
+++ src/scripting/mod.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 07/08/2026
-//! Files   : nexsiz/src/scripting/mod.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::scripting::mod
 //!
 //! NEXSIZ – Python scripting / RPC campaign control surface
 
