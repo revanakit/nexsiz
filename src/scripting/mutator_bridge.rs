ea2a0cb380f75ceaaa5b1e55ff353088020eabf4
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:18:50 +0700

    Update mutator_bridge.rs

--- src/scripting/mutator_bridge.rs
+++ src/scripting/mutator_bridge.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 07/08/2026
-//! Files   : nexsiz/src/scripting/mutator_bridge.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::scripting::mutator_bridge
 //!
 //! Python Mutator hooks bridge (push extras – zero hot-path RPC)
 //! 
