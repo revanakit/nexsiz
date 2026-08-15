784cf090883c159bacf8cd40d837b5743ebe88b8
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 13:04:46 +0700

    Revise comments in linux.rs for clarity
    Updated author and module comments for consistency.

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 14/08/2026
-//! Module  : nexsiz::src::platform::linux
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::platfrom::linux
 //!
 //! Linux POSIX shared-memory coverage provider.
 //!
