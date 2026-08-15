ded9f1b2afe11c380970904b1f8762eb06ae9845
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 10:53:24 +0700

    Update author and module comments in provider.rs

--- src/coverage/provider.rs
+++ src/coverage/provider.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 12/08/2026
-//! Module  : nexsiz::src::coverage::provider
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::coverage::provider
 //!
 //! Coverage provider abstraction and related types.
 //!
