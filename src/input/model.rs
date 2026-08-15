9c416f2b4c3d513c9a1470d7d5d7d038fa1e9e6d
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 12:56:34 +0700

    Revise comments in model.rs
    Updated author and module comments in model.rs

--- src/input/model.rs
+++ src/input/model.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 13/08/2026
-//! Module  : nexsiz::src::input::model
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::input::model
 //!
 //! This module provides an in-memory, minimal but expressive representation of
 //! network protocol structure used by the fuzzer core. It contains:
