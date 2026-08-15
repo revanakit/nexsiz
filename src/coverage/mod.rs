d6cb7f7aaa3341dd3cf107ec00b681b50b1b191b
Revana Clarestya <reva.reane@gmail.com>
2026-08-15 10:52:12 +0700

    Revise author and module comments in mod.rs
    Updated author and module information in mod.rs

--- src/coverage/mod.rs
+++ src/coverage/mod.rs
@@ -1,8 +1,7 @@
 //! NEXSIZ – NEXT-GENERATION STATEFUL NETWORK PROTOCOL FUZZER
 //!
-//! Author  : Revana
-//! Date    : 14/08/2026
-//! Files   : nexsiz/src/coverage/mod.rs
+//! AUTHOR     ::     Revana 
+//! MODULE     ::     src::coverage::mod
 //!
 //! NEXSIZ – Grey-box Coverage / Instrumentation layer
 //!
